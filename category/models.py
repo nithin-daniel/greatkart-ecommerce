e30ccd969b2b90a60a2f3fdc3a22f668d9c03f7d
Nithin Daniel <nithindaniel2018@gmail.com>
2022-05-24 19:07:37 +0530

    context_processor added

--- category/models.py
+++ category/models.py
@@ -1,8 +1,5 @@
-from distutils.command.upload import upload
-from tabnanny import verbose
-from unicodedata import category
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=50,unique=True)
@@ -13,6 +10,10 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+
+    def get_url(self):
+            return reverse('products_by_category',args=[self.slug])
 
     def __str__(self):
         return self.category_name
