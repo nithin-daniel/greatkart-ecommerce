efb667bc38cb0fdbdad755eb8db43385e8589937
Nithin Daniel <nithindaniel2018@gmail.com>
2022-06-07 22:47:36 +0530

    variation model registered in admin.py

--- store/admin.py
+++ store/admin.py
@@ -5,5 +5,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name','price','stoke','category','modified_date','is_available')
     prepopulated_fields = {'slug':('product_name',)}
 
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product','variation_category','variation_value','is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product','variation_category','variation_value',)
+
+
+
+
+
 admin.site.register(Product,ProductAdmin)
-admin.site.register(Variation)
+admin.site.register(Variation,VariationAdmin)
--- store/models.py
+++ store/models.py
@@ -24,6 +24,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+
+        
     
 variation_category_choice = (
     ('color','color'),
@@ -38,7 +42,7 @@ class Variation(models.Model):
     is_active                = models.BooleanField(default=True)
     created_date             = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.product
     
 
