d8ffdfe533d7f0ab3bc41ed56ee4f9c32cb3fc82
Nithin Daniel <nithindaniel2018@gmail.com>
2022-06-13 17:11:20 +0530

    displayed cart items in checkout page

--- carts/views.py
+++ carts/views.py
@@ -146,5 +146,26 @@ def cart(request, total=0, quantity=0, cart_item=None):
 
 
 
-def checkout(request):
-    return render(request, 'store/checkout.html')
+def checkout(request,total=0, quantity=0, cart_item=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart_items = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total/100)
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass #just ignore
+
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+        'tax':tax,
+        'grand_total':grand_total,
+    }
+    return render(request, 'store/checkout.html',context)
