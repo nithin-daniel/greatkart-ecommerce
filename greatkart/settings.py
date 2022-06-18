75604bf8a87d1470f9cb682f3620f779cfd6eb70
Nithin Daniel <nithindaniel2018@gmail.com>
2022-06-18 13:42:09 +0530

    paypal payment integration added

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -152,3 +152,6 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Razorpay Payment Gateway
+
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('cart/',include('carts.urls')),
     path('accounts/',include('accounts.urls')),
     path('orders/',include('orders.urls')),
+    # path('payments/',include('payments.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
