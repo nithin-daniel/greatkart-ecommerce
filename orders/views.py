b3a4c0cd65ad0771f8cf9736324a5b057c696657
Nithin Daniel <nithindaniel2018@gmail.com>
2022-06-19 21:27:02 +0530

    payment transcation history send to backend

--- orders/views.py
+++ orders/views.py
@@ -3,13 +3,33 @@ from django.http import HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm
 import datetime
-from .models import Order
-
+from .models import Order,Payment
+import json
 # Create your views here.
 
 
 def payments(request):
+    print('call received here')
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user,is_ordered=False,order_number=body['orderID'])
 
+    # Store transcation details inside Payment model
+
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    
+    
     return render(request, 'orders/payments.html')
 
 
