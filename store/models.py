4987057a25907ef7bd713a0c69ebca2ebe14186d
Nithin Daniel <nithindaniel2018@gmail.com>
2022-06-27 19:26:57 +0530

    Store the Rating & Reviews

--- store/models.py
+++ store/models.py
@@ -55,18 +55,22 @@ class Variation(models.Model):
     
 
 class ReviewRating(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE,null=True)
     subject = models.CharField(max_length=100,blank=True)
     review = models.TextField(max_length=500,blank=True)
-    rating = models.FloatField()
+    rating = models.FloatField(null=True,blank=True)
     ip = models.CharField(max_length=20,blank=True)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.product
+
+
+
+
+    # def __str__(self):
+    #     return self.product.product_name
     
 
 
