40ed2d4acbca09707fad62e707a577064aff87e4
Nithin Daniel <nithindaniel2018@gmail.com>
2022-06-27 21:15:59 +0530

    Check if the User has purchased the Product before submitting Review

--- store/views.py
+++ store/views.py
@@ -8,6 +8,7 @@ from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from django.http import HttpResponse
 from .forms import ReviewForm
 from django.contrib import messages
+from orders.models import OrderProduct
 # Create your views here.
 def store(request,category_slug=None):
     categories = None
@@ -40,10 +41,16 @@ def product_detail(request,category_slug,product_slug):
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
     except Exception as e:
         raise e 
-     
+    
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user,product_id = single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
+
     context = {
         'single_product':single_product,
         'in_cart'       :in_cart,
+        'orderproduct' : orderproduct,
     }
     return render(request, 'store/product_detail.html',context)
 
@@ -67,7 +74,7 @@ def submit_review(request,product_id):
     if request.method == 'POST':
         try:
             reviews = ReviewRating.objects.get(user__id = request.user.id, product__id = product_id)
-            form = Reviewform(request.POST, instance= reviews)
+            form = ReviewForm(request.POST, instance= reviews)
             form.save()
             messages.success(request, 'Thank you! Your review has been updated')
             return redirect(url)
