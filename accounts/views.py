decbcd11123ca4afb9ce1845c17cad0f1ad4942c
Nithin Daniel <nithindaniel2018@gmail.com>
2022-07-01 22:48:11 +0530

    Fix Profile Picture in Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -161,8 +161,11 @@ def dashboard(request):
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
     orders_count = orders.count()
 
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+
     context = {
         'orders_count' : orders_count,
+        'userprofile' : userprofile,
 
     }
 
