65e8b0c1bb690f15e89aad39c14779eb9daaa908
Nithin Daniel <nithindaniel2018@gmail.com>
2022-07-02 22:07:24 +0530

    Store Your Website Sensitive Information Securely

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jxkn!!v(&&5c(3u2r*m-t=mxxkkvqqw46h0tsmo##m9h_zl_84'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -142,11 +142,11 @@ MESSAGE_TAGS = {
 
 
 #  SMTP configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = '' # Your Email
-EMAIL_HOST_PASSWORD = '' # Your Password , Video Link = 'https://www.youtube.com/watch?v=qk8nJmIRbxk&t=289s&ab_channel=WebTech
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT' ,cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER',) # Your Email
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') # Your Password , Video Link = 'https://www.youtube.com/watch?v=qk8nJmIRbxk&t=289s&ab_channel=WebTech
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
